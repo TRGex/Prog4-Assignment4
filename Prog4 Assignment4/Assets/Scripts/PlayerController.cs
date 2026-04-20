c1969e25c30b3c94a70c6d06fb906bf922a24a5c
TRGex <148483731+TRGex@users.noreply.github.com>
2026-04-19 20:57:16 -0400

    Character controller
    Player can now walk around in a very basic way.

--- Prog4 Assignment4/Assets/Scripts/PlayerController.cs
+++ Prog4 Assignment4/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 5f;
-    Rigidbody rb;
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    public Rigidbody rb;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void MovementInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+    private void Move()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.transform.Translate(moveDirection * 0.25f);
     }
 }
